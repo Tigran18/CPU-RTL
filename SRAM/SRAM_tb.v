ab9696967ab48634abf05fc1ae8f40446a5c49c3
Tigran18 <tigrandavtyan900@gmail.com>
2024-12-22 11:54:06 +0400

    Works properly

--- SRAM/SRAM_tb.v
+++ SRAM/SRAM_tb.v
@@ -9,7 +9,7 @@ reg [7:0] addr;
 reg [31:0] data_in;
 wire [31:0] data_out;
 
-always #2 clk = ~clk; 
+always #1 clk = ~clk; 
 
 SRAM u_sram(
     .clk(clk),
@@ -24,37 +24,31 @@ initial begin
     $dumpfile("SRAM.vcd");
     $dumpvars(0, SRAM_tb);
     clk = 0;
-    res = 1; 
-    addr = 0; 
-    data_in = 0; 
-    WE = 0;
-    
+    res = 1;  
     #5; 
     res = 0;
-
-    for (addr = 0; addr < 32; addr = addr + 1) begin
-        #5;  
+    for (addr = 0; addr < 33; addr = addr + 1) begin
+        #5;
+        WE = 0;  
         if (addr <= 15) begin
             data_in = 32'b0;
             data_in[15 - addr] = 1;  
             data_in[15 + addr] = 1;  
-        end else begin
+        end 
+        else begin
             data_in = 32'b0;
             data_in[addr - 15] = 1;  
-            data_in[31 - (addr - 15)] = 1;  
+            data_in[32 - (addr - 15)] = 1;  
         end
-        
         #5;
-        WE = 1;
-        #5;
-        WE = 0;  
+        WE = 1;  
     end
 
     $finish;  
 end
 
 initial begin
-    $monitor("Time: %0t | Addr: %0d | Data In: %b | Data Out: %b", $time, addr, data_in, data_out);
+    $monitor("Data Out: %b", data_out);
 end
 
 endmodule
